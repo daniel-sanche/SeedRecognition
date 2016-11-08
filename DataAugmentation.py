84f5db1749f23a7e54c5437c756a9a18b0bec2d0
Daniel Sanche <d.sanche14@gmail.com>
2016-11-08 14:21:01 -0600

    added comments

--- DataAugmentation.py
+++ DataAugmentation.py
@@ -3,6 +3,16 @@ import numpy as np
 from scipy.misc import imread, imsave, imresize
 from skimage import exposure
 
+"""
+combines a portion of the imageMat ino an image file that can be displayed
+
+Params:
+    imageMat:   the numpy array of images we want to display
+    numRows:    the number of rows to display
+    numCols:    the number of columns to display
+    fileName:   the name to save the output file with
+    maxImgSize: the size of each side of each block in the image grid
+"""
 def visualizeImages(imageMat, numRows=5, numCols=10, fileName="images.png", maxImgSize=64):
     imageSize = [imageMat.shape[1], imageMat.shape[2], imageMat.shape[3]]
     CombinedImage = np.ones([imageSize[0]*numRows, imageSize[1]*numCols, imageSize[2]])
@@ -24,7 +34,16 @@ def visualizeImages(imageMat, numRows=5, numCols=10, fileName="images.png", maxI
         CombinedImage = imresize(CombinedImage, [maxImgSize*numRows, maxImgSize*numCols])
     imsave(fileName, CombinedImage)
 
+"""
+loads a set of images from a directory. Will find all .png files in any nested directory
+Images will be stored as floats between 0 and 1
 
+Params:
+    imageDir:   the root directory to find images in
+    imageSize:  images will be resized to this size
+Returns:
+    0:  a numpy array of images, stacked on the first dimension
+"""
 def getImagesFromDir(imageDir, imageSize=[640, 700, 3]):
     pathList = []
     for root,dir, files in os.walk(imageDir):
@@ -41,15 +60,41 @@ def getImagesFromDir(imageDir, imageSize=[640, 700, 3]):
         i=i+1
     return imageMat / 255
 
-def gammaColorChannels(imageMat, R=True, G=True, B=True):
-    if R:
-        imageMat[:,:,:,0] = np.power(imageMat[:,:,:,0],(2.2))
-    if G:
-        imageMat[:, :, :, 1] = np.power(imageMat[:, :, :, 1], (2.2))
-    if B:
-        imageMat[:, :, :, 2] = np.power(imageMat[:, :, :, 2], (2.2))
+"""
+appplies a gamma transformation to color channels
 
-def makeGaussian(size=100, radius=50, center=None):
+Params:
+    imageMat:   the numpy array of images to adjust
+    R:          indicates whether to adjust the red channel
+    G:          indicates whether to adjust the green channel
+    B:          indicates whether to adjust the blue channel
+
+Returns:
+    0:  a numpy array of the new adjusted images
+"""
+def gammaColorChannels(imageMat, R=True, G=True, B=True):
+    resultMat = np.array(imageMat)
+    if R:
+        resultMat[:,:,:,0] = np.power(imageMat[:,:,:,0],(2.2))
+    if G:
+        resultMat[:, :, :, 1] = np.power(imageMat[:, :, :, 1], (2.2))
+    if B:
+        resultMat[:, :, :, 2] = np.power(imageMat[:, :, :, 2], (2.2))
+    return resultMat
+
+
+"""
+helper function to make a radial gradient mask with the proper attributes
+
+Params:
+    size:   the size of the sides of the square mask
+    radius: the size of the radius of the radial gradient
+    center: the center point of the gradient
+
+Returns:
+    0:  a size*size grayscale gradient image, that can be treated as a mask
+"""
+def _makeGradMask(size=100, radius=50, center=None):
     x = np.arange(0, size, 1, float)
     y = x[:,np.newaxis]
 
@@ -60,18 +105,55 @@ def makeGaussian(size=100, radius=50, center=None):
         y0 = center[1]
     return np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / radius**2)
 
+"""
+Adds simulated lighting in the form of a radial gradient mask to a set of images
+
+Params:
+    imageMat:   the numpy array of images to add lighting to
+    radPercent: the size of the radius of the light, in relation to the size of the image
+                larger radi are preferred to avoid excessive vignette effect
+    center:     the center point of the simulated light on the image
+
+Returns:
+    0:  a numpy array consisting of imageMat with a graidnet mask applied to simulate a light source
+"""
 def addLighting(imageMat, radPercent=1.1, center=None):
     largerSide = max(imageMat.shape[1], imageMat.shape[2])
-    grad = makeGaussian(largerSide, radius=largerSide*radPercent, center=center)
+    grad = _makeGradMask(largerSide, radius=largerSide * radPercent, center=center)
     grad = imresize(grad, [imageMat.shape[1], imageMat.shape[2]])
     grad = np.tile(grad, (3,1,1)).transpose(1,2,0)
     return imageMat * grad
 
+"""
+Adds gaussian noise to a set of images
+
+Params:
+    imageMat:   the numpy array of images to add noise to
+    mean:       the mean of the gaussian noise distribution
+    std:        the standard deviation of the gaussian noise
+
+Returns:
+    0:  a numpy array consisting of imageMat with gaussian noise added
+"""
 def addGausianNoise(imageMat, mean=0, std=0.1):
     noise = np.random.normal(mean, std, [imageMat.shape[1], imageMat.shape[2], imageMat.shape[3]])
     return imageMat + noise
 
-def adjustContrast(imageMat, minIntensity=0, maxIntensity=1):
+
+"""
+Adjusts the contrast values for a set of images
+
+Params:
+    imageMat:       the input numpy array of images
+    meanIntensity:  the new mean intensity value
+    range:          the range of values on either side of the mean
+
+Returns:
+    0:  a numpu array consisting of imageMat scaled to the new intensity values
+"""
+def adjustContrast(imageMat, meanIntensity=0.5, range=0.5):
+    minIntensity = max(meanIntensity - range, 0)
+    maxIntensity = min(meanIntensity + range, 1)
     return exposure.rescale_intensity(imageMat, (minIntensity, maxIntensity))
 
 if __name__ == "__main__":
@@ -79,7 +161,7 @@ if __name__ == "__main__":
     imageMat = getImagesFromDir(imageDir, imageSize=[100, 100, 3])
     #visualizeImages(imageMat, fileName="orig.png")
     #gammaColorChannels(imageMat)
-    imageMat = adjustContrast(imageMat, 0.2, 0.8)
+    imageMat = adjustContrast(imageMat, 0.7, 0.5)
     visualizeImages(imageMat, fileName="dark_seeds.png")
 
 
