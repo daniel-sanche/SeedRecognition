846d420a9099e2695ac66b5d316b33328ee475db
Daniel Sanche <d.sanche14@gmail.com>
2017-02-27 16:11:14 -0600

    changed test script to use all test image folders

--- testing.py
+++ testing.py
@@ -4,15 +4,22 @@ import os
 import numpy as np
 
 if __name__ == "__main__":
-    imageDir = "/home/sanche/Datasets/Seed_Test/p4_45_first/"
+    imageRoot = "/home/sanche/Datasets/Seed_Test"
 
     vgg = vgg16('vgg16_weights.npz')
     batchSize = 45
     saveInterval = float("inf")
 
-    i = 0
-    fileReader = DataLoader.groundTruthReader(os.path.join(imageDir, 'p4_groundtruth.txt'))
-    for imageBatch, classBatch in DataLoader.batchLoader(imageDir, fileReader,batchSize=batchSize):
-        predictions, successRate = vgg.test(imageBatch, classBatch.reshape([batchSize,]))
-        result = (classBatch.reshape([batchSize]), predictions, successRate)
-        print("truth: %s, prediction: %s successRate: %f" % result)
+    filesCount = 0
+    successCount = 0.0
+    for dirNum in range(1,7):
+        thisDir = "p" + str(dirNum) + "_45_first"
+        imageDir = os.path.join(imageRoot, thisDir)
+        fileReader = DataLoader.groundTruthReader(os.path.join(imageDir, 'p'+str(dirNum)+'_groundtruth.txt'))
+        for imageBatch, classBatch in DataLoader.batchLoader(imageDir, fileReader,batchSize=batchSize):
+            predictions, success = vgg.test(imageBatch, classBatch.reshape([batchSize,]))
+            result = (classBatch.reshape([batchSize]), predictions, success)
+            print("truth: %s, prediction: %s successRate: %f" % result)
+            filesCount += batchSize
+            successCount += (batchSize * success)
+    print ("total success: %f in %d files" % (successCount/filesCount, filesCount))
