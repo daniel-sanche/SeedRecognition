7ab1b1fbcd598af1194a4fa6fdd46f268599558f
Daniel Sanche <daniel@sanche.space>
2017-03-04 12:08:32 -0600

    load weights for pre-trained network

--- kerasVGG.py
+++ kerasVGG.py
@@ -9,59 +9,79 @@ from keras.applications import vgg16
 from keras import backend as K
 from quiver_engine import server
 from keras.models import Sequential
-from keras.layers import Convolution2D, ZeroPadding2D, MaxPooling2D, Flatten, Dense
+from keras.layers import Convolution2D, ZeroPadding2D, MaxPooling2D, Flatten, Dense, Dropout
 from flask import jsonify
-
-img_width, img_height = 224, 224
+import h5py
 
 # build the VGG16 network
-model = Sequential()
-model.add(ZeroPadding2D((1, 1), batch_input_shape=(1, img_width, img_height, 3), name="input"))
-first_layer = model.layers[-1]
-# this is a placeholder tensor that will contain our generated images
-input_img = first_layer.input
+class VGG:
+    def __init__(self):
+        model = Sequential()
+        model.add(ZeroPadding2D((1, 1), input_shape=(224, 224, 3)))
+        model.add(Convolution2D(64, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(64, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-# build the rest of the network
-model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(128, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(128, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(256, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(ZeroPadding2D((1, 1)))
+        model.add(Convolution2D(512, 3, 3, activation='relu'))
+        model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
-model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(4096, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(4096, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(30, activation='softmax'))
+        self.model = model
 
-model.add(Flatten())
-model.add(Dense(4096, init='glorot_uniform', activation="relu", bias=True, input_dim=25088, name="fc1"))
-model.add(Dense(4096, init='glorot_uniform', activation="relu", bias=True, input_dim=4096, name="fc2"))
-model.add(Dense(30, init='glorot_uniform', activation="softmax", bias=True, input_dim=4096, name="output"))
+    # weights location: https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3
+    def loadWeights(self, weights_path):
+        f = h5py.File(weights_path)
+        for k in range(f.attrs['nb_layers']):
+            if k >= len(self.model.layers) - 1:
+                # we don't look at the last two layers in the savefile (fully-connected and activation)
+                break
+            g = f['layer_{}'.format(k)]
+            weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+            layer = self.model.layers[k]
+            if layer.__class__.__name__ in ['Convolution1D', 'Convolution2D', 'Convolution3D', 'AtrousConvolution2D']:
+                weights[0] = np.transpose(weights[0], (2, 3, 1, 0))
 
+            layer.set_weights(weights)
 
-#model = vgg16.VGG16(weights='imagenet', include_top=True)
-server.launch(model, classes=[str(i) for i in range(30)])
+        f.close()
+        print('Model loaded.')
+
+    def display(self):
+        server.launch(self.model, classes=[str(i) for i in range(30)])
+
+vggModel = VGG()
+vggModel.loadWeights("vgg16_weights_keras.h5")
+vggModel.display()
